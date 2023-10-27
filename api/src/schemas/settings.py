60e81f5727fe4cc5b6299a36b373c856f17bc8c3
Inácio Oliveira <inacioigne@gmail.com>
2023-10-27 16:26:29 -0400

    27/10/2023

--- api/src/schemas/settings.py
+++ api/src/schemas/settings.py
@@ -3,6 +3,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "BiblioKeia"
     url: str = "http://localhost"
-    fuseki: str = "http://192.168.128.4:3030" 
-    mariadb: str = "192.168.128.2"
-    solr: str = "http://192.168.128.3:8983"
+    # fuseki: str = "http://192.168.128.4:3030" 
+    fuseki: str = "http://172.24.0.3:3030" 
+    mariadb: str = "172.24.0.2"
+    solr: str = "http://172.23.0.2:8983"
+    # mariadb: str = "192.168.128.2"
+    # solr: str = "http://192.168.128.3:8983"
